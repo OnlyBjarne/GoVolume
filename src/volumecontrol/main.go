ad941e0afb9c263d3288641b80ef1dd799478d46
Bjarne <bjakke112@gmail.com>
2018-11-15 17:25:09 +0100

    Restructured config file
    compressed and need more coffee when first made

--- src/volumecontrol/main.go
+++ src/volumecontrol/main.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/getlantern/systray"
 )
-
-//SettingsStruct to populate html from json config file
-type SettingsStruct struct {
-	Settings []struct {
-		Encoder []struct {
-			Knob []struct {
-				Text        string   `json:"text"`
-				Application []string `json:"Application"`
-				Active      bool     `json:"Active"`
-			} `json:"Knob,omitempty"`
-			Button []struct {
-				Text        string   `json:"text"`
-				Application []string `json:"Application"`
-				Active      bool     `json:"Active"`
-			} `json:"Button,omitempty"`
-		} `json:"encoder"`
-	} `json:"Settings"`
-}
 
 func main() {
 	systray.Run(onReady, onExit)
@@ -46,9 +26,8 @@ func onReady() {
 	//Add server for settings-manager in webapp
 	indexHandler := http.NewServeMux()
 	indexHandler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		p := getSettings()
 		t, _ := template.ParseFiles("SettingsTemplate.html")
-		t.Execute(w, p)
+		t.Execute(w, nil)
 	})
 	indexHandler.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Saved")
@@ -63,9 +42,10 @@ func onReady() {
 		select {
 		case <-mOpen.ClickedCh:
 			fmt.Println("Config clicked")
-			go log.Fatal(srv.ListenAndServe())
+			log.Fatal(srv.ListenAndServe())
 		case <-mQuit.ClickedCh:
 			fmt.Println("Quitting")
+			srv.Close()
 			systray.Quit()
 		}
 	}
@@ -80,20 +60,4 @@ func getIcon(s string) []byte {
 		fmt.Print(err)
 	}
 	return b
-}
-
-func getSettings() SettingsStruct {
-	settingsFile, err := os.Open("settings.cfg.default")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer settingsFile.Close()
-
-	byteValue, err := ioutil.ReadAll(settingsFile)
-	var settings SettingsStruct
-
-	json.Unmarshal(byteValue, &settings)
-	fmt.Println(settings.Settings[0].Encoder[1].Knob[1].Text)
-	return settings
 }
